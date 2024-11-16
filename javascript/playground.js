1623322cfde88786fa96be57c2a45344a5e4d26c
RAHUL WARKADE <88571832+Rahulwarkade@users.noreply.github.com>
2024-11-16 20:44:05 +0000

    type conversions

--- javascript/playground.js
+++ javascript/playground.js
@@ -1,12 +1,23 @@
 "use strict";
-// Data types
-//number => 2 to power 53
-// bigint
-// string =>""
-// null 
-// undefined
-// symbol => unique
 
-// object
+// let valueInNumber = Number("33a");
+// console.table([valueInNumber+3, typeof valueInNumber]);
 
-console.log( typeof (typeof null))
+// "33" =>33 number
+// "33abc" => NaN
+// true =>1; false=>0;
+// Null => 0
+
+// Truthy and falsey
+// false, NaN, '', null, 0, -0, 0n, undefined,
+
+// let isLoggedIn = [];
+// let valueInNumber = Boolean(isLoggedIn);
+
+// console.table([valueInNumber,typeof valueInNumber]);
+
+let someNumber = 33;
+
+let someString = String(33);
+
+console.table([someString,typeof someString]);
